b712b8bd1647a5c3d03f266467ebfd3574dd520e
Shivaprasad1216 <aravireddy12@gmail.com>
2026-09-07 17:12:29 +0530

    Initial commit

--- assignment.py
+++ assignment.py
@@ -50,4 +50,53 @@ print(a>=18 and b<=60)
 #10
 maths=80
 science=35
+print(maths>=35 and science>=35)
 
+#11
+x=True
+print(not x)
+
+#12
+Age=25
+Qualification="Degree"
+print(Age>=18 and Qualification=="Degree")
+
+#13
+languages=["Java", "Python", "C++", "SQL"]
+print("Python" in languages)
+
+#14
+languages=["python","Java","C++"]
+print("Javascript" not in languages)
+
+#15
+name="Aparna"
+print("a"in name)
+
+#16
+a=[1,2,3]
+b=a
+print(a is b)
+
+#17
+a=[1,2,3]
+b=[1,2,3]
+print(a is not b)
+
+#18
+x=20
+x+=10
+print(x)
+
+#19
+x=10
+x*=5
+print(x)
+
+#20
+
+x+=20
+x-=10
+x*=2
+x/=2
+print(x)
